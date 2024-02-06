5593f5fd62b57df6851fe5735ec92e6620d0b30c
siddhrajparmar1302 <145355072+siddhrajparmar1302@users.noreply.github.com>
2024-02-06 15:05:07 +0530

    Add files via upload

---
+++ Assignment/3.2/3.2{27- i}.c.c
@@ -0,0 +1,29 @@
+// WAP to show Monday to Sunday using switch case
+#include<stdio.h> 
+int main() 
+ { 
+   int choice; 
+   printf("Enter the number");
+     printf("\n1.Monday\n2.Tuesday\n3.Wednesday\n4.Thursday\n5.Friday\n6.Saturday\n7.Sunday\n");
+   scanf("%d",&choice); 
+   switch(choice) 
+   { 
+   case 1 : printf("\n Monday"); 
+   break; 
+   case 2 : printf("\n Tuesday"); 
+   break; 
+   case 3 : printf("\n Wednesday"); 
+   break; 
+   case 4 : printf("\n Thursday"); 
+   break; 
+   case 5 : printf("\n Friday"); 
+   break; 
+   case 6 : printf("\n Saturday"); 
+   break; 
+   case 7 : printf("\n Sunday"); 
+   break; 
+   default : printf("\n enter correct choice"); 
+   break;
+   } 
+   return 0; 
+ }
---
+++ Assignment/3.2/3.2{27-ii}.c.c
@@ -0,0 +1,47 @@
+
+// WAP to show vowel or consonant using switch case
+#include <stdio.h>
+int main()
+{
+    char choice;
+
+    /* Input an alphabet from user */
+    printf("Enter any alphabet: ");
+    scanf("%c", &choice);
+    switch(choice)
+    {
+        case 'a': 
+            printf("Vowel");
+            break;
+        case 'e': 
+            printf("Vowel");
+            break;
+        case 'i': 
+            printf("Vowel");
+            break;
+        case 'o': 
+            printf("Vowel");
+            break;
+        case 'u': 
+            printf("Vowel");
+            break;
+        case 'A': 
+            printf("Vowel");
+            break;
+        case 'E': 
+            printf("Vowel");
+            break;
+        case 'I': 
+            printf("Vowel");
+            break;
+        case 'O': 
+            printf("Vowel");
+            break;
+        case 'U': 
+            printf("Vowel");
+            break;
+        default: 
+            printf("Consonant");
+    }
+    return 0;
+}     
